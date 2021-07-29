4a66dbc6654a6410356fb00609710b2be1d3d2c4
Viktoriia <maksymovychvika@gmail.com>
2021-07-30 00:21:51 +0300

    hw2 part2

--- js.js
+++ js.js
@@ -18,4 +18,26 @@ for (let key in citiesAndCountries) {
 	result.push(key + " - это " + citiesAndCountries[key])
 }
 
-console.log(result)
+//console.log(result)
+
+/*Task 2: Создать функцию которая выведет многомерный массив A.
+ Данный массив содержит в себе список других массивов B.
+ Массивы B должны содержать по 3 значения.
+ Максимальное значение (в примере это переменная amount) должно делится на 3 нацело.*/
+
+const getArray = (amount) => {
+
+	if (amount < 3 || amount % 3 !== 0) {
+		console.log("Type number multiple of three")
+		return
+	}
+	const amountOfArr = amount / 3
+	let counter = 1
+	let resultArr = []
+	for (let i = 0; i <= amountOfArr; i++) {
+		for (let j = 0; j < 3; j++) {
+			resultArr[i] = counter
+			counter++
+		}
+	}
+}
