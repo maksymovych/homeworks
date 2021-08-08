e02ac7ab6c99e7850aada46bceb6f6b899a9ad83
Viktoriia <maksymovychvika@gmail.com>
2021-08-08 17:43:27 +0300

    Task1

--- js.js
+++ js.js
@@ -7,38 +7,40 @@ class Student {
 		this.ratingPoint = enrollee.ratingPoint
 		this.schoolPoint = enrollee.schoolPoint
 		this.id = Student.getId()
-		this.isSelfPayment = Student.getPayment(this.ratingPoint)
-		Student.addStudent(this)
+		this.isSelfPayment = Student.getPayment(this, this.ratingPoint, this.schoolPoint)
 	}
+
 
 	static id = 1
 	static getId() {
 		return this.id++
 	}
 
-	static getPayment(rating) {
-		if (rating >= 800 && this.listOfStudents.length < 5) {
+	static listOfStudents = []
+	static getPayment(student, currentRating, currentSchoolPoint) {
+		const listLangth = this.listOfStudents.length
+		if (currentRating >= 800 && listLangth < 5) {
+			this.listOfStudents.push(student)
 			return false
-		} else if (rating >= 800 && this.listOfStudents.length >= 5) {
-			//	const index = this.
+		} else if (currentRating >= 800 && listLangth >= 5) {
+			this.listOfStudents.sort((a, b) => a.ratingPoint - b.ratingPoint)
+			let pointIndex = this.listOfStudents.findIndex(item => item.ratingPoint < currentRating)
+			if (pointIndex === -1) {
+				this.listOfStudents.sort((a, b) => a.schoolPoint - b.schoolPoint)
+				pointIndex = this.listOfStudents.findIndex(item => item.ratingPoint < currentSchoolPoint)
+
+			}
+			this.listOfStudents.splice(pointIndex, 1, student)
+			return pointIndex !== -1 ? false : true
 		}
 		return true
 	}
 
-	static listOfStudents = []
-	static addStudent(student) {
-		if (!student.isSelfPayment) {
-			this.listOfStudents.push(student)
-		}
-
-	}
-
 	get listOfStudents() {
-		// if(!this.isSelfPayment){}
 		return this.listOfStudents
 	}
 }
-const students = []
-for (let i = 0; i < 10; i++) {
-	students[i] = new Student(studentArr[i])
+
+for (let i = 0; i < studentArr.length; i++) {
+	new Student(studentArr[i])
 }
