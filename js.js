61340b03d1e9d3ae404d17343f2d6c15e581ff2a
Viktoriia <maksymovychvika@gmail.com>
2021-08-15 15:54:41 +0300

    Task4

--- js.js
+++ js.js
@@ -44,3 +44,43 @@ const setTimer = () => {
 const getTime = setTimer()
 
 //Task 4
+
+const timer = time => {
+	const isAvailableTime = () => {
+		if (time >= 1 && time < 6040) {
+			return false
+		}
+		console.log("Time is anavailable. Must be number more then '0' and les then '6040' seconds")
+		return true
+	}
+
+	if (isAvailableTime()) return
+
+	time = Math.floor(time)
+
+	const SECONDS_IN_MINUT = 60
+	let minutes = 0
+	let seconnds = 0
+
+	const timeLog = (t) => {
+		const getMinutes = (m) => {
+			let min = Math.floor(m / SECONDS_IN_MINUT)
+			if (min === 0) return "00"
+			if (min < 10) return "0" + min
+			return min
+		}
+		const getSeconds = (s) => {
+			return 0
+		}
+		console.log(getMinutes(t) + ":" + getSeconds(t))
+	}
+
+	for (let i = time; i >= 0; i--) {
+		const timerToConsole = timeLog(i)
+		setInterval(timerToConsole, 1000)
+		if (i === 0) {
+			clearInterval(timerToConsole)
+			return "Timer End"
+		}
+	}
+}
