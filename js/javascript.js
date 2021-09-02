adc1fec02be3ac89be38435a1253d371b3cb032f
Viktoriia <maksymovychvika@gmail.com>
2021-09-02 07:08:21 +0300

    hw8 html+css

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+(true + "") === "true"
+4 && 5 == '5' && ++num || !0
+
+("12" < "true") + parseFloat('13.5$')
+!!parseFloat("t123") - false + true
+
+String(false) - String(true) + "not" + NaN
+("text" > 'Text') + 5 + null * 45
+
+"true" + (Boolean('false') / 0)
+(undefined == null) + + '12' + '13' / 1
