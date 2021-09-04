2be87a0a3461b246c7bf58c3b4e683f0b02b3afa
Viktoriia <maksymovychvika@gmail.com>
2021-09-04 15:45:53 +0300

    task1

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //Task 1
-const numberOfFields =   7//prompt('Pleas enter number of fields:')
+const numberOfFields = prompt('Pleas enter number of fields:')
 
-const button = document.querySelector('.button')
-const yellow = 
+const buttonRegistr = document.querySelector('.button')
 
 Array(numberOfFields).fill(1).forEach((item, index)=>{
+debugger
 	const input = document.createElement('input')
 	input.value = `Input ${index + 1}`
-
 	input.className = index < numberOfFields - 1 ? 'input-item' : 'input-item margin-zero'
+
 	!(index % 2) ? input.classList.add('yellow') : ''
 
 	if (!((index + 1) % 3)){
@@ -16,5 +16,8 @@ Array(numberOfFields).fill(1).forEach((item, index)=>{
 		input.placeholder = 'Some text'
 	} 
 	
-	button.before(input)
+	buttonRegistr.before(input)
 })
+
+//Task 2
+//const button = document.createElement('button')
