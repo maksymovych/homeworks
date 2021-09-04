18baa67b89f689f84a588a2f6e97aa2f20db751c
Viktoriia <maksymovychvika@gmail.com>
2021-09-05 00:15:45 +0300

    tas3,4

--- script.js
+++ script.js
@@ -53,3 +53,64 @@ function timer(event){
 
 	event.target.value === 'startTimer' ? timerId = setInterval(getTime, 1000) : clearInterval(timerId)
 }
+
+//Task 3
+function changColor(){
+	const lastP = document.body.querySelector('#main').lastElementChild
+	lastP.style.backgroundColor = 'red'
+	return
+}
+
+function changeBlocks(){
+	const footer = document.querySelector('#footer')
+	const main = document.querySelector('#main')
+	const wrapper = document.querySelector('#wrapper')
+	wrapper.prepend(main)
+	wrapper.append(footer)
+	return
+}
+
+changColor()
+changeBlocks()
+
+//Task 4
+const list = document.querySelector('#menu')
+list.addEventListener('click', numerableList)
+let checker = true
+
+function numerableList(event){
+	const INGREDIENTS = {
+		"cocoa": ["cocoa powder", "milk", "sugar"],
+		"cappuccino": ["milk", "coffee"], 
+		"smoothie": ["banana", "orange", "sugar"],
+		"matcha frappe": ["matcha", "milk", "ice"]
+	}
+	
+	let ol = document.createElement('ol')
+
+	function showRecipe(){
+		const target = event.target
+		const innerText = event.target.innerText
+		target.after(ol)
+		INGREDIENTS[innerText].forEach(item=> {
+			const li = document.createElement('li')
+			li.innerText = item
+			ol.append(li)
+		})
+	}
+
+	function closeRecipe(){
+		document.querySelectorAll('ol > li').forEach(item=>item.remove())
+		ol.remove()
+	}
+
+	if (checker){
+		showRecipe()
+	} else {
+		closeRecipe()
+	}
+	
+	checker = !checker
+
+	return
+}
