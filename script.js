344f3cf0cd220bba088044fb886dff2a51c1b690
Viktoriia <maksymovychvika@gmail.com>
2021-09-06 19:58:31 +0300

    www

--- script.js
+++ script.js
@@ -17,7 +17,45 @@ let firstPromise = new Promise((resolve, reject)=>{
 })
 
 //Task 2
-function goToShop(){
-	let productAmount = null
-	return productAmount
+function makeDinner(){
+	return new Promise((resolve, reject) => {
+		setTimeout(console.log('Bon appetite'), 3000)
+		resolve()
+	})
 }
+function goToShop(){
+	
+	return new Promise(resolve=>{
+		resolve()
+	}).then(response=>{
+		console.log(response)
+	})
+}
+
+//Task 3
+let container = document.body.querySelector('.container')
+fetch('https://rickandmortyapi.com/api/character/?page=1')
+.then((response) => response.json())
+.then((data) => data.results.map(item => {
+
+	const card = `
+	<div class="card">
+		<div class="card-info">
+			<div class="title">
+				<h1>${item.name}</h1>
+				<div class="status">
+					<div class="live-status"></div>
+					<p>${item.species}</p>
+				</div>
+			</div>
+			<div class="content">
+				<p>${item.location.name}</p>
+			</div>
+		</div>
+		<div class="card-image">
+			<img src=${item.image}>
+		</div>
+	</div>
+`
+	container.innerHTML += card
+}));
