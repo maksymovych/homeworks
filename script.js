bed7073c08dc20ebc195b60dca2e2c883834879f
Viktoriia <maksymovychvika@gmail.com>
2021-09-07 12:41:43 +0300

    task3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Task 1
-let firstPromise = new Promise((resolve, reject)=>{
+/*let firstPromise = new Promise((resolve, reject)=>{
 	setTimeout(()=>{
 		const number = Math.ceil(Math.random() * 6)
 		if (number === 6) {
@@ -31,31 +31,80 @@ function goToShop(){
 		console.log(response)
 	})
 }
-
+*/
 //Task 3
 let container = document.body.querySelector('.container')
-fetch('https://rickandmortyapi.com/api/character/?page=1')
-.then((response) => response.json())
-.then((data) => data.results.map(item => {
 
-	const card = `
-	<div class="card">
-		<div class="card-info">
-			<div class="title">
-				<h1>${item.name}</h1>
-				<div class="status">
-					<div class="live-status"></div>
-					<p>${item.species}</p>
+document.forms[0].addEventListener('click', filter())
+
+async function getApi(path){
+	let api = 'https://rickandmortyapi.com/api/character/'
+	const response = await fetch(`${api}${path}`)
+
+	return await response.json()
+}
+
+function getCharactersById(...path){
+	getApi(path.toString())
+	.then(data => {
+		if (path.length > 1){
+			data.map(item => addCardToDOM(item))
+		} else {
+			addCardToDOM(data)
+		}
+	})
+	.catch(error => {
+		throw new Error('Some error: ' + error)
+	})
+}
+
+function filter(){
+	let filterPath = '?&'
+	return (target) =>{
+		const value = target.target.id
+		if (!value) return
+
+		let key = (value === 'male' || value === 'female') ? 'gender' : ''
+		key = (value === 'alive' || value === 'dead') ?  'status' : key
+		
+		
+		if (target.target.checked){
+			filterPath += `&${key}=${value}`
+		} else {
+			filterPath = filterPath.replace(`&${key}=${value}`, '')
+		}
+
+		getApi(filterPath)
+		.then(array=>{
+			console.log(array.results)
+			container.innerHTML =''
+			array.results.map(character => addCardToDOM(character))
+		})
+	}
+	
+}
+
+function addCardToDOM(item){
+
+	const status = item.status === 'Alive' ? 'live-status' : 'live-status dead'
+	container.innerHTML += `
+		<div class="card">
+			<div class="card-info">
+				<div class="title">
+					<h1>${item.name}</h1>
+					<div class="status">
+						<div class=${status}></div>
+						<p>${item.species}</p>
+					</div>
+				</div>
+				<div class="content">
+					<p>${item.location.name}</p>
 				</div>
 			</div>
-			<div class="content">
-				<p>${item.location.name}</p>
+			<div class="card-image">
+				<img src=${item.image}>
 			</div>
 		</div>
-		<div class="card-image">
-			<img src=${item.image}>
-		</div>
-	</div>
-`
-	container.innerHTML += card
-}));
+	`
+	return
+}
