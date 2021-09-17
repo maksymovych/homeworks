a93a1e0ead926f87378dd76dbc9855c530b1ce10
Viktoriia <maksymovychvika@gmail.com>
2021-09-17 16:02:14 +0300

    refactor3

--- functions.js
+++ functions.js
@@ -1,74 +1,97 @@
-const events = document.body.querySelector('.events')
+const events = document.body.querySelector('.events');
+const eventWidth = 198
+let id = null;
+
 
 function addTimeAgenda(){
-	const agenda = document.body.querySelector('.agenda')
+	const agenda = document.body.querySelector('.agenda');
 	for(let i = 8; i < 17; i++){
-		const hr = document.createElement('hr')
-		const span1 = document.createElement('span')
-		const span2 = document.createElement('span')
-		span1.classList = ('time big')
+		const hr = document.createElement('hr');
+		const span1 = document.createElement('span');
+		const span2 = document.createElement('span');
+		span1.classList = ('time big');
 		span2.classList = ('time smole')
-		span1.textContent = `${i}:00`
-		span2.textContent = `${i}:30`
-		agenda.append(hr)
-		agenda.append(span1)
-		agenda.append(span2)
+		span1.textContent = `${i}:00`;
+		span2.textContent = `${i}:30`;
+		agenda.append(hr);
+		agenda.append(span1);
+		agenda.append(span2);
 	}
-	const hr = document.createElement('hr')
-	agenda.append(hr)
+	const hr = document.createElement('hr');
+	agenda.append(hr);
 }
-let id = null
 
 function makeEvent(obj){
-	const event = document.createElement('div')
-	const spanClose = document.createElement('span')
-	spanClose.innerText = 'x'
-	spanClose.classList.add('removeEvent')
-	spanClose.setAttribute('id', id)
-	event.setAttribute('id', id++)
-	event.classList.add('event')
-	event.textContent = obj.title
-	event.append(spanClose)
-	event.style.width = obj.width + 'px'
-	event.style.height= obj.duration + 'px'
-	event.style.top = obj.start + 'px'
-	event.style.right = obj.right + 'px'
-	return event
+	const event = document.createElement('div');
+	const spanClose = document.createElement('span');
+	spanClose.innerText = 'x';
+	spanClose.classList.add('removeEvent');
+	spanClose.setAttribute('id', id);
+	event.setAttribute('id', id++);
+	event.classList.add('event');
+	event.textContent = obj.title;
+	event.append(spanClose);
+	event.style.width = obj.width + 'px';
+	event.style.height= obj.duration + 'px';
+	event.style.top = obj.start + 'px';
+	event.style.right = obj.right + 'px';
+	return event;
 }
 
-function putOneTimeEvent(obj){
-	const event = makeEvent({...obj, width: 198, right: 0})
-	events.append(event)
+function putOneTimeEvent(obj, width = eventWidth, right = 0){
+	const event = makeEvent({...obj, width, right});
+	events.append(event);
 }
 
 function putMultipleEvent(obj){
 	obj.forEach((element, index) => {
-		const size = 200 / obj.length
+		const size = eventWidth / obj.length;
 		const event = makeEvent({...element, 
 			width: size - 2,
 			right: size * index})
-			events.append(event)
+			events.append(event);
 		});
 }
-	
+
+function hostinMultipleevents(arr){
+	const last = arr[arr.length - 1];
+	const prev = arr[arr.length - 2]
+	const prevEventFinish = prev.start + prev.duration
+
+	if (prevEventFinish <= last.start){
+		putMultipleEvent(arr.filter((i, index)=> index < arr.length - 1))
+		const right = eventWidth / (arr.length - 1) ;
+		const width = eventWidth - right;
+		putOneTimeEvent(last, width, right)
+		return
+	}
+	if(arr.length === 3){
+		putMultipleEvent(arr.filter((i, index)=> index < arr.length - 1))
+		const width = (eventWidth / 2) - 2;
+		putOneTimeEvent(last, width)
+		return
+	}
+	putMultipleEvent(arr)
+	return
+}
 
 function multipleCurrentEvents(accum, current, index, arr){
-	const accumDur = accum[accum.length -1].start + accum[accum.length -1].duration
-
+	const accumDur = accum[accum.length -1].start + accum[accum.length -1].duration;
+	const isSomeEentCross = accum.some(item=> item.start + item.duration > current.start)
 	if (index === arr.length - 1 && accumDur <= current.start){
-		putMultipleEvent(accum)
-		putOneTimeEvent(current)
-		return
+		putMultipleEvent(accum);
+		putOneTimeEvent(current);
+		return;
 	}
 	if (index === arr.length - 1 && accumDur > current.start){
-		putMultipleEvent([...accum, current])
-		return
+		putMultipleEvent([...accum, current]);
+		return;
 	}
-	if (accumDur <= current.start){
-		putMultipleEvent(accum)
-		return current
+	if (accumDur <= current.start && !isSomeEentCross){
+		hostinMultipleevents(accum)	
+		return current	
 	} 
-	return [...accum, current]
+	return [...accum, current];
 }
 
 function singleCurrentEvent(accum, current, index, arr){
@@ -95,8 +118,7 @@ function addEvents(events){
 	const currentTime = ((new Date().getHours() - 8) * 60) + new Date().getMinutes()
 	events.reduce((accum, current, index, arr)=>{
 		const currentDur = current.start + current.duration
-		const accumDur = accum.start + accum.duration
-		if (currentDur > 540 || current.start < 0 || index === 0 && (accum.start < 0 || accumDur > 540)){
+		if (currentDur > 540 || current.start < 0){
 			throw new Error('All events should had been started at 8:00 and finished by 17:00')
 		}
 		return Array.isArray(accum) ? multipleCurrentEvents(accum, current, index, arr) 
@@ -117,10 +139,9 @@ function addNewEvent(start, duration, title){
 }
 	
 function changeColor(e){
-	const color = prompt('Type color f.e. (0, 242, 255)')
-	e.target.style.backgroundColor = `rgba(${color}, 0.5)`
-	e.target.style.borderLeft = `2px solid rgb(${color})`
-	console.log(e.target.style.borderLeft)
+	const color = prompt('Type color f.e. (0, 242, 255)');
+	e.target.style.backgroundColor = `rgba(${color}, 0.5)`;
+	e.target.style.borderLeft = `2px solid rgb(${color})`;
 }
 
 
--- js.js
+++ js.js
@@ -1,19 +1,22 @@
 let agenda = [
 	{start: 0, duration: 35, title: 'Exersize at 8 oclock'},
 	{start: 25, duration: 20, title: 'Wea to work'},
-	{start: 60, duration: 30, title: 'Mitng with teem'},
-	{start: 65, duration: 45, title: 'Code rewiew'},
+	{start: 60, duration: 135, title: 'Mitng with teem'},
+	{start: 65, duration: 50, title: 'Code rewiew'},
 	{start: 100, duration: 35, title: 'Skype call'},
 	{start: 180, duration: 25, title: 'Plan Day'},
 	{start: 400, duration: 20, title: 'Have lanch with Kate'},
+	{start: 450, duration: 20, title: 'HSomething with Kate'}
 	
-]
+];
+
 //max 540
 
 
-addTimeAgenda()
+addTimeAgenda();
 
-addEvents(agenda)
+
+addEvents(agenda);
 
 
 document.forms.add.addEventListener('submit',
